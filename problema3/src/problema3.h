5871445937c195c3f92141c722604337ddf47656
ealmansi <ealmansi@gmail.com>
2014-05-03 17:49:50 -0300

    problema3: src autoformat + edit chiquito en leerEntrada + reordeno includes

--- problema3/src/problema3.h
+++ problema3/src/problema3.h
@@ -2,44 +2,41 @@
 #define PROBLEMA_3_H
 
 #include <list>
-#include <utility>
-#include <iostream>
 #include <vector>
-
-using namespace std;
 
 namespace Problema3
 {
-	struct Casillero{
-		int f;
-		int c;
-		int k;
-		Casillero(){;}
-		Casillero(int f,int c, int k)
-			:f(f),c(c),k(k) {}	
+	struct Casillero
+	{
+	  int f;
+	  int c;
+	  int k;
+	  Casillero() {}
+	  Casillero(int f, int c, int k)
+	    : f(f), c(c), k(k) {}
 	};
+
 	struct Entrada
 	{
 	  int n;
-	  int k_max;
-	  vector< vector < int > > tablero;
 	  Casillero origen;
 	  Casillero destino;
-	  
+	  int k_max;
+	  std::vector< std::vector < int > > tablero;
 	};
 
 	struct Salida
 	{
 	  int saltos;
-	  list< Casillero > caminoMinimo;
+	  std::list< Casillero > caminoMinimo;
 	};
 
 	Entrada leerEntrada();
 
-	Salida resolver(Entrada& e);
+	Salida resolver(Entrada &e);
 
-	void imprimoSalida(Salida& s);
-	list< Casillero > losAdyacentes(const Entrada& e, const Casillero& c);
+	void imprimoSalida(Salida &s);
+	std::list< Casillero > losAdyacentes(const Entrada &e, const Casillero &c);
 }
 
 #endif /* PROBLEMA_3_H */
--- problema3/src/resolver.cpp
+++ problema3/src/resolver.cpp
@@ -1,146 +1,165 @@
 #include "problema3.h"
-#include <climits>
+
+#include <iostream>
 #include <queue>
+#include <utility>
+#include <climits>
 
 using namespace std;
 using namespace Problema3;
 
-typedef vector< pair< Casillero, int > > Matriz; 
-typedef vector< Matriz > Matriz2D; 
-typedef vector< Matriz2D > Matriz3D; 
+typedef vector< pair< Casillero, int > > Matriz1D;
+typedef vector< Matriz1D > Matriz2D;
+typedef vector< Matriz2D > Matriz3D;
 
-Entrada Problema3::leerEntrada(){
-	Entrada e;
-	cin >> e.n;
-	int fo, co, fd, cd ,k;
-	cin >> fo;
-	cin >> co;
-	cin >> fd;
-	cin >> cd;
-	cin >> k;
-	e.origen = Casillero(fo,co,k);
-	e.destino = Casillero(fd,cd,0);
-	e.k_max = k;
-	///hago tablero
-	e.tablero.push_back(vector< int >(e.n+1));
-	for(int i = 1; i <= e.n; ++i){
-		e.tablero.push_back(vector< int >(e.n+1));
-		e.tablero[i].push_back(-1);
-		for(int j = 1; j <=e.n;++j){
-			int temp;
-			cin >> temp;
-			e.tablero[i][j] = temp;
-		}
-	}
-	
-	return e;
+Entrada Problema3::leerEntrada()
+{
+  Entrada e;
+  
+  int n, fo, co, fd, cd , k;
+  cin >> n;
+  cin >> fo;
+  cin >> co;
+  cin >> fd;
+  cin >> cd;
+  cin >> k;
+  
+  e.n = n;
+  e.origen = Casillero(fo, co, k);
+  e.destino = Casillero(fd, cd, 0);
+  e.k_max = k;
+  
+  int tablero_ij;
+  e.tablero = vector< vector< int > >(e.n + 1, vector< int >(e.n + 1, -1));
+  for (int i = 1; i <= e.n; ++i)
+  {
+    for (int j = 1; j <= e.n; ++j)
+    {
+      cin >> tablero_ij;
+      e.tablero[i][j] = tablero_ij;
+    }
+  }
+
+  return e;
 }
 
-/*
-pair< Casillero, int >* damePos(const Casillero& c, const pair < Casillero, int> & yaPase[][][]){
-	return &yaPase[c.f][c.c][c.k];	
-}
-*/
+Salida Problema3::resolver(Entrada &e)
+{
+  Salida s;
 
+  ///inicializo matriz de 3 dimensiones. Cada celda tiene un casillero y un int que representa la distancia minima desde el origen
+  ///-1 significa que todavia no llegue
+  Matriz3D yaPase(e.n + 1, Matriz2D(e.n + 1, Matriz1D(e.k_max + 1, make_pair(Casillero(0, 0, 0), -1))));
+  //  Matriz3D yaPase(e.k_max+1, Matriz2D(e.n+1, Matriz(e.n+1, make_pair(Casillero(0,0,0), -1))));
+  ///casilleros van de 1 a n.
 
-Salida Problema3::resolver(Entrada& e){
-	Salida s;
-	
-	///inicializo matriz de 3 dimensiones. Cada celda tiene un casillero y un int que representa la distancia minima desde el origen
-	///-1 significa que todavia no llegue
-	Matriz3D yaPase(e.n+1, Matriz2D(e.n+1, Matriz(e.k_max+1, make_pair(Casillero(0,0,0), -1))));
-	//	Matriz3D yaPase(e.k_max+1, Matriz2D(e.n+1, Matriz(e.n+1, make_pair(Casillero(0,0,0), -1))));
-	///casilleros van de 1 a n.
-	
-	///BFS
-	queue< Casillero > q;
-	yaPase[e.origen.f][e.origen.c][e.origen.k].second = 0;
-	int kFinal;
-	q.push(e.origen);
-	
-	while(!q.empty()){
-		Casillero c = q.front();
-		q.pop();
-		if(c.c == e.destino.c && c.f == e.destino.f){
-			///llegamos a la que queriamos
-			kFinal = c.k;
-			break;
-		}
-		
-		list< Casillero > adyacentes = losAdyacentes(e, c);
-		list< Casillero >::iterator itAdy = adyacentes.begin();
-		for(;itAdy != adyacentes.end(); ++itAdy){
-			if(yaPase[itAdy->f][itAdy->c][itAdy->k].second == -1){
-				yaPase[itAdy->f][itAdy->c][itAdy->k].first = c;
-				yaPase[itAdy->f][itAdy->c][itAdy->k].second = yaPase[c.f][c.c][c.k].second + 1;
-				q.push(*itAdy);
-			}
-		}
-	}
-	
-	s.saltos = yaPase[e.destino.f][e.destino.c][kFinal].second;
-	Casillero padre(e.destino.f, e.destino.c, kFinal);
-	for(int i = 0; i <= s.saltos; ++i){
-		s.caminoMinimo.push_front(padre);
-		padre = yaPase[padre.f][padre.c][padre.k].first;
-	}
-	
-	return s;
-}
+  ///BFS
+  queue< Casillero > q;
+  yaPase[e.origen.f][e.origen.c][e.origen.k].second = 0;
+  int kFinal;
+  q.push(e.origen);
 
-list< Casillero > Problema3::losAdyacentes(const Entrada& e, const Casillero& c){
-	list< Casillero > ady;
-	int potencia = e.tablero[c.f][c.c];
-	///estos son sin usar los K del jugador
-	for(int i = 1; i <= potencia && i <= n; ++i){
-		///abajo
-		if(c.f+i <= e.n){
-			ady.push_back(Casillero(c.f+i, c.c, c.k));
-		}
-		///arriba
-		if(c.f-i > 0){
-			ady.push_back(Casillero(c.f-i, c.c, c.k));
-		}
-		///derecha
-		if(c.c+i <= e.n){
-			ady.push_back(Casillero(c.f, c.c+i, c.k));
-		}
-		///izquierda
-		if(c.c-i > 0){
-			ady.push_back(Casillero(c.f, c.c-i, c.k));
-		}
-	}
-	///usando Kaes
-	for(int i = 1; i <= c.k && i <= n; ++i){
-		///abajo
-		if(c.f+i+potencia <= e.n){
-			ady.push_back(Casillero(c.f+i+potencia, c.c, c.k-i));
-		}
-		///arriba
-		if(c.f-i-potencia > 0){
-			ady.push_back(Casillero(c.f-i-potencia, c.c, c.k-i));
-		}
-		///derecha
-		if(c.c+i+potencia <= e.n){
-			ady.push_back(Casillero(c.f, c.c+i+potencia, c.k-i));
-		}
-		///izquierda
-		if(c.c-i-potencia > 0){
-			ady.push_back(Casillero(c.f, c.c-i-potencia, c.k-i));
-		}
-	}
-	
-	return ady; 
+  while (!q.empty())
+  {
+    Casillero c = q.front();
+    q.pop();
+    if (c.c == e.destino.c && c.f == e.destino.f)
+    {
+      ///llegamos a la que queriamos
+      kFinal = c.k;
+      break;
+    }
+
+    list< Casillero > adyacentes = losAdyacentes(e, c);
+    list< Casillero >::iterator itAdy = adyacentes.begin();
+    for (; itAdy != adyacentes.end(); ++itAdy)
+    {
+      if (yaPase[itAdy->f][itAdy->c][itAdy->k].second == -1)
+      {
+        yaPase[itAdy->f][itAdy->c][itAdy->k].first = c;
+        yaPase[itAdy->f][itAdy->c][itAdy->k].second = yaPase[c.f][c.c][c.k].second + 1;
+        q.push(*itAdy);
+      }
+    }
+  }
+
+  s.saltos = yaPase[e.destino.f][e.destino.c][kFinal].second;
+  Casillero padre(e.destino.f, e.destino.c, kFinal);
+  for (int i = 0; i <= s.saltos; ++i)
+  {
+    s.caminoMinimo.push_front(padre);
+    padre = yaPase[padre.f][padre.c][padre.k].first;
+  }
+
+  return s;
 }
 
+list< Casillero > Problema3::losAdyacentes(const Entrada &e, const Casillero &c)
+{
+  list< Casillero > ady;
+  int potencia = e.tablero[c.f][c.c];
+  ///estos son sin usar los K del jugador
+  for (int i = 1; i <= potencia && i <= e.n; ++i)
+  {
+    ///abajo
+    if (c.f + i <= e.n)
+    {
+      ady.push_back(Casillero(c.f + i, c.c, c.k));
+    }
+    ///arriba
+    if (c.f - i > 0)
+    {
+      ady.push_back(Casillero(c.f - i, c.c, c.k));
+    }
+    ///derecha
+    if (c.c + i <= e.n)
+    {
+      ady.push_back(Casillero(c.f, c.c + i, c.k));
+    }
+    ///izquierda
+    if (c.c - i > 0)
+    {
+      ady.push_back(Casillero(c.f, c.c - i, c.k));
+    }
+  }
+  ///usando Kaes
+  for (int i = 1; i <= c.k && i <= e.n; ++i)
+  {
+    ///abajo
+    if (c.f + i + potencia <= e.n)
+    {
+      ady.push_back(Casillero(c.f + i + potencia, c.c, c.k - i));
+    }
+    ///arriba
+    if (c.f - i - potencia > 0)
+    {
+      ady.push_back(Casillero(c.f - i - potencia, c.c, c.k - i));
+    }
+    ///derecha
+    if (c.c + i + potencia <= e.n)
+    {
+      ady.push_back(Casillero(c.f, c.c + i + potencia, c.k - i));
+    }
+    ///izquierda
+    if (c.c - i - potencia > 0)
+    {
+      ady.push_back(Casillero(c.f, c.c - i - potencia, c.k - i));
+    }
+  }
 
-void Problema3::imprimoSalida(Salida& s){
-	cout << s.saltos << endl;
-	list< Casillero >::iterator itPadre = s.caminoMinimo.begin();
-	list< Casillero >::iterator itCamino = s.caminoMinimo.begin();
-	++itCamino;
-	for(;itCamino != s.caminoMinimo.end(); ++itCamino){
-		cout << itCamino->f << " " << itCamino->c << " " << itPadre->k - itCamino->k << endl;
-		++itPadre;
-	}
+  return ady;
+}
+
+
+void Problema3::imprimoSalida(Salida &s)
+{
+  cout << s.saltos << endl;
+  list< Casillero >::iterator itPadre = s.caminoMinimo.begin();
+  list< Casillero >::iterator itCamino = s.caminoMinimo.begin();
+  ++itCamino;
+  for (; itCamino != s.caminoMinimo.end(); ++itCamino)
+  {
+    cout << itCamino->f << " " << itCamino->c << " " << itPadre->k - itCamino->k << endl;
+    ++itPadre;
+  }
 }
